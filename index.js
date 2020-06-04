3221109e887c7fb2a0f603734b3f97ce5e6eff44
Felix <felix.mattick@gmail.com>
2020-06-03 22:36:04 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require('fs')
+const asar = require('asar')
+const path = require('path')
+const prompts = require('prompts')
+const chalk = require('chalk')
+const semverSort = require('semver-sort')
+
+const tempPrefix = 'volcano-temp-' 
+
+const patchAsar = async (asarPath) => {
+  const asarExtractTemp = await fs.promises.mkdtemp(tempPrefix)
+  await asar.extractAll(asarPath, asarExtractTemp)
+
+  const indexPath = path.join(asarExtractTemp, 'index.html')
+  const indexContent = await fs.promises.readFile(indexPath)
+  await fs.promises.writeFile(indexPath, indexContent.toString().replace(
+    '<script type="text/javascript" src="app.js"></script></body>',
+    '<script type="text/javascript" src="app.js"></script><script type="text/javascript" src="volcano.js"></script></body>'
+  ))
+
+  const volcanoContent = await fs.promises.readFile(require.resolve('./volcano.js'))
+  await fs.promises.writeFile(path.join(asarExtractTemp, 'volcano.js'), volcanoContent)
+
+  await fs.promises.unlink(asarPath)
+  await asar.createPackage(asarExtractTemp, asarPath)
+
+  await fs.promises.rmdir(asarExtractTemp, { recursive: true })
+}
+
+const getLatestAsarPath = async (asPath) => {
+  try {
+    const filePaths = await fs.promises.readdir(asPath)
+    const versions = filePaths
+      .filter((path) => path.match(/^obsidian-\d+\.\d+\.\d+.asar$/))
+      .map((path) => path.slice(9, -5))
+
+    if (versions.length > 0) {
+      const sorted = semverSort.desc(versions)
+      return `obsidian-${sorted[0]}.asar`
+    } else if (filePaths.includes('obsidian.asar')) {
+      return 'obsidian.asar'
+    }
+  } catch {}
+}
+
+const getInitialAsarPath = async () => {
+  if (process.platform === 'darwin') {
+    const asPath = path.join(process.env.HOME, 'Library/Application Support/obsidian')
+    const latestAsarPath = await getLatestAsarPath(asPath)
+    if (latestAsarPath) return path.join(asPath, latestAsarPath)
+
+    return '/Applications/Obsidian.app/Contents/Resources/obsidian.asar'
+  } else if (process.platform === 'win32') {
+    const asPath = path.join(process.env.APPDATA, 'obsidian')
+    const latestAsarPath = await getLatestAsarPath(asPath)
+    if (latestAsarPath) return path.join(asPath, latestAsarPath)
+
+    return path.resolve(process.env.APPDATA, '..\\Local\\Obsidian\\resources')
+  }
+}
+
+const go = async () => {
+  const response = await prompts({
+    type: 'text',
+    name: 'asarPath',
+    message: 'What is the path to obsidian.asar?',
+    required: true,
+    initial: await getInitialAsarPath()
+  })
+  if (!response.asarPath) return
+
+  console.log(chalk.blue('Patching asar...'))
+  await patchAsar(response.asarPath)
+  console.log(chalk.green('Done! Launch Obsidian to get started.'))
+}
+
+go()
---
+++ volcano.js
@@ -0,0 +1,60 @@
+(async () => {
+  // Wait until app is ready...
+  // Is there a better way to do this?
+  await new Promise((resolve) => {
+    if (window.app) resolve(window.app)
+    const interval = setInterval(() => {
+      if (window.app) {
+        clearInterval(interval)
+        resolve(window.app)
+      }
+    }, 200)
+  }) 
+
+  const electron = require('electron')
+  const fs = require('fs')
+  const path = require('path')
+
+  const volcanoPath = path.join(electron.remote.app.getPath('home'), 'volcano')
+  const pluginsPath = path.join(volcanoPath, 'plugins')
+
+  const log = (message, error) => {
+    console.log(`%c[Volcano]%c ${message}`, `color: ${error ? '#fa5252' : '#228be6'};`, '')
+    if (error && typeof error !== 'boolean') console.error(error)
+  }
+
+  const findPlugin = (id) => app.plugins.plugins.find((plugin) => plugin.plugin.id === id)
+
+  const loadPlugin = (pluginFile) => {
+    log(`Loading ${pluginFile}...`)
+    try {
+      const getPlugin = require(path.join(pluginsPath, pluginFile))
+
+      const SettingTab = findPlugin('daily-notes').settingTab.constructor
+      const plugin = getPlugin({ findPlugin, SettingTab })
+
+      app.plugins.loadPlugin(plugin)
+
+      if (plugin.defaultOn && !plugin.enabled) {
+        log(`Enabling ${pluginFile} because defaultOn is truthy`)
+        findPlugin(plugin.id).enable(app)
+      }
+
+      log(`Loaded ${pluginFile}`)
+    } catch (error) {
+      log(`Error loading ${pluginFile}`, error)
+    }
+  }
+
+  log(`Making sure ${pluginsPath} exists...`)
+  await fs.promises.mkdir(pluginsPath, { recursive: true })
+
+  log('Loading plugins...')
+
+  const pluginFiles = await fs.promises.readdir(pluginsPath)
+  for (let pluginFile of pluginFiles) {
+    loadPlugin(pluginFile)
+  }
+
+  log('Process completed')
+})()
